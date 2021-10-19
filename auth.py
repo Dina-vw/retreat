817705df9b1bf76442a7ef4c13e396881f01cd1a
dvw <dina.werder@campus.lmu.de>
2021-10-19 10:04:39 +0200

    added auth function

--- auth.py
+++ auth.py
@@ -1,10 +1,25 @@
 
 def get_credentials():
 	username = input('Type username: ')
-	password = input('Type password: ')
+	password = input('Type password: ') 
 	return username, password
 
+def authenticate(username, password, pwdb):
+    auth = False
+    if username in pwdb: 
+        if password == pwdb[username]:
+            print('Authentication successful')
+            auth = True
+        else: 
+            print('Wrong password!')
+    else: 
+        print('User not known')
+    return auth
+    
 
+pwdb = {"dina":"hello", 
+        "phil":"password"}
 
 username, password = get_credentials()
-print(username, password)
+authenticate(username, password, pwdb)
+
