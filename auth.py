70e39165abde3f6ac13c337df6a1b6ebe3709e7e
dvw <dina.werder@campus.lmu.de>
2021-10-19 11:01:07 +0200

    new changes

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-
+import json
 def get_credentials():
 	username = input('Type username: ')
 	password = input('Type password: ') 
@@ -17,9 +17,18 @@ def authenticate(username, password, pwdb):
     return auth
     
 
-pwdb = {"dina":"hello", 
-        "phil":"password"}
+
+
+def write_pwdb(pwdb):
+    with open('pwdb.json', 'wt') as pwdb_file:  
+        jsonson.dump(pwdb, pwdb_file)
+    print('pwdb written!')
+   
+def read_pwdb():
+    with open('pwdb.json', 'rt') as pwdb_file:
+        pwdb = json.load(pwdb_file)
+    return pwdb
 
 username, password = get_credentials()
+pwdb = read_pwdb()
 authenticate(username, password, pwdb)
-
