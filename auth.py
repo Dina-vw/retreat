b64027b85543d1da39f25818d055286a74c5036d
dvw <dina.werder@campus.lmu.de>
2021-10-19 09:39:34 +0200

    first commit

---
+++ auth.py
@@ -0,0 +1,10 @@
+
+def get_credentials():
+	username = input('Type username: ')
+	password = input('Type password: ')
+	return username, password
+
+
+
+username, password = get_credentials()
+print(username, password)
